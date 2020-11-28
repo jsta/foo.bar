4db2985103f523e62334df460b91792e39fcb19a
jsta <stachel2@msu.edu>
2020-11-28 16:26:32 -0500

    indentation fix

--- 01_task.py
+++ 01_task.py
@@ -10,22 +10,22 @@ def solution(s):
     # s = "ababcab"
     # s = "abccbaabccba" # 2    
 
-len_search = 0
-res = []    
+    len_search = 0
+    res = []    
 
-while len_search < len(s):
-    len_search += 1 # if substrings not equal try splitting by last 2 char
-    # print(len_search)
-    last_char = s[(len(s)-len_search):(len(s))]
-    s_split = re.split(str(last_char), s) # try splitting by it
-    s_split = s_split[0:(len(s_split)-1)]
-    s_split = [seq + last_char for seq in s_split]
-    if all(seq == s_split[0] for seq in s_split):
-        res.append(len(s_split))
-    else:
-        res.append(0)
+    while len_search < len(s):
+        len_search += 1 # if substrings not equal try splitting by last 2 char
+        # print(len_search)
+        last_char = s[(len(s)-len_search):(len(s))]
+        s_split = re.split(str(last_char), s) # try splitting by it
+        s_split = s_split[0:(len(s_split)-1)]
+        s_split = [seq + last_char for seq in s_split]
+        if all(seq == s_split[0] for seq in s_split):
+            res.append(len(s_split))
+        else:
+            res.append(0)
 
-return max(res)
+    return max(res)
     
 
 solution(s = "abab")
